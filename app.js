b4814ecc82164be690eb829c715ff74053d364dd
basithalikp <sithukp50@gmail.com>
2026-07-24 15:43:53 +0530

    Fixed URL in app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Configuration for remote data source
-const DATA_SOURCE_URL = process.env.DATA_SOURCE_URL || 'https://ik.imagekit.io/acmnssce/acm/data.json';
+const DATA_SOURCE_URL = process.env.DATA_SOURCE_URL || 'https://raw.githubusercontent.com/basithalikp/acm-events-cdn/refs/heads/main/data.json';
 const CACHE_DURATION = parseInt(process.env.CACHE_DURATION) || 30000; // 5 minutes default
 const USE_LOCAL_FALLBACK = process.env.USE_LOCAL_FALLBACK !== 'true'; // true by default
 
